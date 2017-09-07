cd41a37142a81855c7d7727fbd5cd2ab721abb8c
alvaro <amianalvarez@gmail.com>
2017-09-07 19:47:40 +0200

    some style

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -10,6 +10,13 @@ class Main extends Component {
         displayName: 'Alvaro Amian',
         username: 'aamian',
         date: Date.now()
+      },
+                {
+        text: 'Hola Caraculo',
+        picture: 'https://abs.twimg.com/icons/apple-touch-icon-192x192.png',
+        displayName: 'Alvaro Amian',
+        username: 'aamian',
+        date: Date.now() - 1800000
       }]
     }
   }
